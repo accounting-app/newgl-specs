7bef3eca75a27ec05754918e7798f92c1eb7cc31
Hector Garcia <hmgarcia06@gmail.com>
2026-06-22 13:23:43 -0400

    Out of the night that covers me,       Black as the pit from pole to pole, I thank whatever gods may be       For my unconquerable soul.
    In the fell clutch of circumstance
          I have not winced nor cried aloud.
    Under the bludgeonings of chance
          My head is bloody, but unbowed.
    
    Beyond this place of wrath and tears
          Looms but the Horror of the shade,
    And yet the menace of the years
          Finds and shall find me unafraid.
    
    It matters not how strait the gate,
          How charged with punishments the scroll,
    I am the master of my fate,
          I am the captain of my soul.

---
+++ qbo_data_stucture/start.ts
@@ -0,0 +1,52 @@
+// ENDPOINT: POST https://qbo.intuit.com/api/neo/v2/company/9341454233117488/olb/manualUpdate/start
+// PAYLOAD: {fiList: []}
+
+export const response = {
+    "hasErrors": false,
+    "isComplete": false,
+    "subJobs": [
+        {
+            "fiId": 8,
+            "fiName": "Chase-2",
+            "jobId": "87425926-402c-433d-960f-f81ebb839725|8",
+            "hasError": false,
+            "isComplete": false,
+            "accounts": [
+                {
+                    "name": "CHASE SAVINGS 7250",
+                    "id": "234",
+                    "hasError": false
+                },
+                {
+                    "name": "CHASE CREDIT 2525",
+                    "id": "235",
+                    "hasError": false
+                },
+                {
+                    "name": "CHASE CHECKING 3623",
+                    "id": "236",
+                    "hasError": false
+                }
+            ]
+        },
+        {
+            "fiId": 7,
+            "fiName": "Chase-1",
+            "jobId": "0d444178-9c7b-4b9e-bce3-8110a6b05118|7",
+            "hasError": false,
+            "isComplete": false,
+            "accounts": [
+                {
+                    "name": "CHASE SECURE CASH 1745",
+                    "id": "222",
+                    "hasError": false
+                },
+                {
+                    "name": "CHASE CREDIT 9416",
+                    "id": "225",
+                    "hasError": false
+                }
+            ]
+        }
+    ]
+}
